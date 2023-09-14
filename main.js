daefbcbc1fd1270aaddbfcb67c724d8269283495
Satvir Singh <stvrdeol@gmail.com>
2023-09-14 13:40:18 +0530

    app is ready for production

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+const mobileMenu = document.querySelector("#mobile-menu");
+const mobileMenuOpen = document.querySelector("#mobile-menu-open");
+const mobileMenuClose = document.querySelector("#mobile-menu-close");
+
+mobileMenuOpen.addEventListener("click", () => {
+  mobileMenu.classList.toggle("translate-x-[200vw]");
+});
+mobileMenuClose.addEventListener("click", () => {
+  mobileMenu.classList.toggle("translate-x-[200vw]");
+});
